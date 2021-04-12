7d2037bfbe1fc667074b02a1d6e4dabaff08e96b
Moatez98 <reh.moatez@gmail.com>
2021-04-12 12:30:14 +0100

    First try

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mail,pass;
+    Button akek;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+            akek=findViewById(R.id.btnlogin);
+            mail=findViewById(R.id.email);
+            pass=findViewById(R.id.password);
+
+            akek.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String m=mail.getText().toString();
+                    String p=pass.getText().toString();
+                    if(m.equals("admin") && p.equals("admin")){
+                        Intent i=new Intent(getApplicationContext(),home.class);
+                        startActivity(i);
+                    }else{
+                        Toast.makeText(getApplicationContext(),"pot de pass ghalet",Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+
+
+
+    }
+
+
+}
